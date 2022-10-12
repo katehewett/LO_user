22e2ea067640c3a447654b777a09ea606a67c214
katehewett <hewett.kate@gmail.com>
2022-10-12 11:45:15 -0700

    Update job_definitions.py

--- extract/box/job_definitions.py
+++ extract/box/job_definitions.py
@@ -68,7 +68,7 @@ def get_box(job, Lon, Lat):
     elif job == 'harcourt':
         aa = [-125.6, -124.2, 46.6, 47.2]
         vn_list = 'h,pm,pn,mask_rho,salt,temp,oxygen,zeta,u,v,w,Uwind,Vwind'
-    elif job == 'NHL1_45'     
+    elif job == 'NHL1_45':
         aa = [-124.0999985, -125.1166992,   45.65169907,   43.65169907]
         vn_list = 'h,f,pm,pn,mask_rho,salt,temp,rho,oxygen,zeta,u,v,Uwind,Vwind' 
     return aa, vn_list
