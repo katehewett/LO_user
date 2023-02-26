d62aa2665ffe876c6d3e48a0ab35cee769ddd8e9
katehewett <hewett.kate@gmail.com>
2023-02-26 01:25:25 -0800

    Update get_one_volume.py

--- extract/hypoxic_volume/get_one_volume.py
+++ extract/hypoxic_volume/get_one_volume.py
@@ -5,23 +5,15 @@ Function to do the calculation for one hypoxic volume for a single history file.
 from argparse import ArgumentParser
 from xarray import open_dataset, Dataset
 from numpy import nan, ones, diff
-#from pickle import dump
-#from pandas import read_pickle, to_datetime
-from time import time
+#from time import time
 from lo_tools import zrfun
 import numpy as np
 
-#import numpy as np
-
 parser = ArgumentParser()
-#parser.add_argument('-sect_df_fn', type=str) # path to sect_df
 parser.add_argument('-in_fn', type=str) # path to history file
 parser.add_argument('-out_fn', type=str) # path to outfile (temp directory)
 parser.add_argument('-lt', '--list_type', default = 'daily', type=str) # list type: hourly, daily, weekly
-#parser.add_argument('-vn_type', type=str) # 'salt' or 'bio'
 args = parser.parse_args()
-
-#hv_df = read_pickle(args.hv_df_fn)
 
 ds = open_dataset(args.in_fn, decode_times=False)
 # the decode_times=False part is important for correct treatment
@@ -68,10 +60,23 @@ z_rho, z_w = zrfun.get_z(h, zeta, S)
 #CC['zeta_rho'] = z_rho
 #CC['zeta_w'] = z_w
 
+# hypoxia thresholds 
 dzr = np.diff(z_w, axis = 0)
 oxy = ds.oxygen.values.squeeze()
-dzrm = np.ma.masked_where(oxy>61,dzr) 
+
+dzrm = np.ma.masked_where(oxy>60.9,dzr) 
 hyp_dz = dzrm.sum(axis=0)
+del dzrm 
+
+dzrm = np.ma.masked_where(oxy>106.6,dzr) 
+mild_dz = dzrm.sum(axis=0)
+del dzrm 
+
+dzrm = np.ma.masked_where(oxy>21.6,dzr) 
+severe_dz = dzrm.sum(axis=0)
+
+dzrm = np.ma.masked_where(oxy>0,dzr) 
+anox_dz = dzrm.sum(axis=0)
 
 # add in anoxia 
 # add in Oag - p, T, S, oxy -> look at LO/daymovie/dm_pfun.py
@@ -86,7 +91,10 @@ ot = ds.ocean_time.values # an array with dtype='datetime64[ns]'
 ds1 = Dataset()
 ds1['ocean_time'] = (('ocean_time'), ot, {'long_name':ds.ocean_time.long_name,'units':ds.ocean_time.units})
 
+ds1['mild_dz'] = (('ocean_time', 'eta_rho', 'xi_rho'), CC['mild_dz'].reshape(1,NR,NC), {'units':'m', 'long_name': 'Thickness of mild hypoxic layer'})
 ds1['hyp_dz'] = (('ocean_time', 'eta_rho', 'xi_rho'), CC['hyp_dz'].reshape(1,NR,NC), {'units':'m', 'long_name': 'Thickness of hypoxic layer'})
+ds1['severe_dz'] = (('ocean_time', 'eta_rho', 'xi_rho'), CC['severe_dz'].reshape(1,NR,NC), {'units':'m', 'long_name': 'Thickness of severe hypoxic layer'})
+ds1['anox_dz'] = (('ocean_time', 'eta_rho', 'xi_rho'), CC['anox_dz'].reshape(1,NR,NC), {'units':'m', 'long_name': 'Thickness of anoxic layer'})
 
 ds1['DA'] = (('eta_rho', 'xi_rho'), CC['DA'], {'units':'m^2', 'long_name': 'cell horizontal area '})
 ds1['mask_rho'] = (('eta_rho', 'xi_rho'), CC['DA'], {'flag_values':[0., 1.],'flag_meanings':'land water','long_name': 'mask on RHO-points'})
