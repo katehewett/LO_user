92a2e39b3b318028db20c9649bdf612da2879123
katehewett <hewett.kate@gmail.com>
2025-08-21 17:40:21 -0700

    Updates to get_lo_info

--- old_get_lo_info.py
+++ old_get_lo_info.py
@@ -21,12 +21,8 @@ LOo = parent / 'LO_output'
 LOu = parent / 'LO_user'
 data = parent / 'LO_data'
 
-# This is a new piece of information, to help with integration of
-# Aurora Leeson's new LO_traps repo, 2023.11.03.
-traps_name = 'traps00'
-# In order for this to be more useful it would have to be integrated
-# into Aurora's code.
-# I'm not sure this is the best way to solve this problem.
+# This is where the ROMS source code, makefiles, and executables are
+roms_code = parent / 'LiveOcean_roms'
 
 # These are places where the ROMS history files are kept
 roms_out = parent / 'LO_roms'
@@ -35,7 +31,7 @@ roms_out2 = parent / 'BLANK'
 roms_out3 = parent / 'BLANK'
 roms_out4 = parent / 'BLANK'
 
-# these are for klone
+# these are for mox and klone, other hyak mackines
 remote_user = 'BLANK'
 remote_machine = 'BLANK'
 remote_dir0 = 'BLANK'
@@ -55,25 +51,30 @@ except KeyError:
 # print('HOME = ' + str(HOME))
 # print('HOSTNAME = ' + HOSTNAME)
 
-if (str(HOME) == '/Users/katehewett'):
-    lo_env = 'kh_mac'
-    which_matlab = '/Applications/MATLAB_R2023b.app/bin/matlab'
-    roms_out1 = Path('/Users/katehewett/Documents/LO_roms') # beeecareful!
+if str(HOME) == '/Users/pm8':
+    lo_env = 'pm_mac'
+    which_matlab = '/Applications/MATLAB_R2020a.app/bin/matlab'
 
-# parker
 elif (str(HOME) == '/home/parker') & ('perigee' in HOSTNAME):
     lo_env = 'pm_perigee'
     roms_out1 = Path('/agdat1/parker/LO_roms')
     roms_out2 = Path('/agdat2/parker/LO_roms')
-    roms_out3 = Path('/data1/auroral/LO_roms')
+    roms_out3 = Path('/data1/parker/LiveOcean_roms/output')
     roms_out4 = Path('/data2/parker/LiveOcean_roms/output')
 
 elif (str(HOME) == '/home/parker') & ('apogee' in HOSTNAME):
     lo_env = 'pm_apogee'
-    roms_out1 = Path('/dat1/parker/LO_roms')
-    roms_out2 = Path('/dat2/parker/LO_roms')
-    roms_out3 = Path('/dat2/jxiong/LO_roms')
-    roms_out4 = Path('/pgdat2/parker/LO_roms')
+    roms_out1 = Path('/pgdat1/parker/LO_roms')
+    roms_out2 = Path('/pgdat2/parker/LO_roms')
+
+elif (str(HOME) == '/usr/lusers/pmacc'):
+    lo_env = 'pm_mox'
+    remote_user = 'parker'
+    # remote_machine = 'perigee.ocean.washington.edu'
+    # remote_dir0 = '/data1/parker'
+    remote_machine = 'apogee.ocean.washington.edu'
+    remote_dir0 = '/dat1/parker'
+    local_user = 'pmacc'
 
 elif ((str(HOME) == '/mmfs1/home/pmacc') or (str(HOME) == '/mmfs1/home/darrd')):
     lo_env = 'pm_klone'
@@ -83,29 +84,33 @@ elif ((str(HOME) == '/mmfs1/home/pmacc') or (str(HOME) == '/mmfs1/home/darrd')):
     local_user = 'pmacc'
 
 # kate
+elif (str(HOME) == '/Users/katehewett'):
+    lo_env = 'kh_mac'
+    which_matlab = '/Applications/MATLAB_R2020a.app/bin/matlab'
+    roms_out1 = Path('/Users/katehewett/Documents/LO_roms') # beeecareful!
+    
 elif (str(HOME) == '/home/kmhewett') & ('perigee' in HOSTNAME):
     lo_env = 'kh_perigee'
     roms_out1 = Path('/agdat1/parker/LO_roms')
     roms_out2 = Path('/agdat2/parker/LO_roms')
-    roms_out3 = Path('/data1/parker/LO_roms')
-    roms_out4 = Path('/data2/parker/LiveOcean_roms/output') # old liveocean
-    #roms_out5 = Path('/data1/kmhewett/LO_roms')             
-
+    roms_out3 = Path('/data1/parker/LO_roms')       # /data1/parker/LO_roms/cas6_v0_live !!
+    roms_out4 = Path('/data2/parker/LO_roms')
+    
 elif (str(HOME) == '/home/kmhewett') & ('apogee' in HOSTNAME):
     lo_env = 'kh_apogee'
-    roms_out1 = Path('/dat1/parker/LO_roms')
+    roms_out1 = Path('/pgdat1/parker/LO_roms')
+    #roms_out2 = Path('/pgdat2/parker/LO_roms')
     roms_out2 = Path('/dat2/parker/LO_roms')
-    roms_out3 = Path('/pgdat1/parker/LO_roms')
-    roms_out4 = Path('/pgdat2/parker/LO_roms')
-    roms_out5 = Path('/dat1/kmhewett/LO_roms')
+    roms_out3 = Path('/dat1/parker/LO_roms')
 
-elif ((str(HOME) == '/mmfs1/home/kmhewett') or (str(HOME) == '/mmfs1/home/darrd')):
+
+elif (str(HOME) == '/mmfs1/home/kmhewett'):
     lo_env = 'kh_klone'
     remote_user = 'kmhewett'
     remote_machine = 'apogee.ocean.washington.edu'
     remote_dir0 = '/dat1/kmhewett'
     local_user = 'kmhewett'
-
+    
 Ldir0 = dict()
 Ldir0['lo_env'] = lo_env
 Ldir0['parent'] = parent
@@ -113,15 +118,17 @@ Ldir0['LO'] = LO
 Ldir0['LOo'] = LOo
 Ldir0['LOu'] = LOu
 Ldir0['data'] = data
+Ldir0['roms_code'] = roms_code
 Ldir0['roms_out'] = roms_out
 Ldir0['roms_out1'] = roms_out1
 Ldir0['roms_out2'] = roms_out2
 Ldir0['roms_out3'] = roms_out3
 Ldir0['roms_out4'] = roms_out4
+Ldir0['which_matlab'] = which_matlab
 #
 Ldir0['remote_user'] = remote_user
 Ldir0['remote_machine'] = remote_machine
 Ldir0['remote_dir0'] = remote_dir0
 Ldir0['local_user'] = local_user
-#
-Ldir0['traps_name'] = traps_name
+
+
