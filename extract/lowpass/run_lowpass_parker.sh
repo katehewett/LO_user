d848e8da27bb2661854239ae52cb142f9df0b53c
katehewett <hewett.kate@gmail.com>
2026-07-24 13:59:07 -0700

    Update run_lowpass_parker.sh

--- extract/lowpass/run_lowpass_parker.sh
+++ extract/lowpass/run_lowpass_parker.sh
@@ -6,3 +6,5 @@ LOe=/dat1/parker/LO/extract/lowpass
 
 python3 $LOe/extract_lowpass.py -gtx cas7_t2_x11b -ro 1 -0 2024.01.01 -1 2024.12.31 -Nproc 4 > lowpass_log2024.log
 python3 $LOe/extract_lowpass.py -gtx cas7_t2_x11b -ro 1 -0 2025.01.01 -1 2025.12.31 -Nproc 4 > lowpass_log2025.log
+
+python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11b -ro 5 -0 2017.01.01 -1 2017.12.31 -Nproc 4 > lowpass_log2017.log 
