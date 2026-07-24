149c6e94a7174aafad0a8e5643ecfc650b730a28
katehewett <hewett.kate@gmail.com>
2026-07-24 13:42:25 -0700

    updates to run files

--- extract/lowpass/run_lowpass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# code to create lowpass files for 2017, 2024 and 2025 (hourly data)
-
-LOe=/dat1/parker/LO/extract/lowpass
-
-python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11ab -ro 1 -0 2017.01.01 -1 2017.12.31 -Nproc 4 > lowpass_log2017.log
-python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11ab -ro 1 -0 2024.01.01 -1 2024.12.31 -Nproc 4 > lowpass_log2024.log
-python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11ab -ro 1 -0 2025.01.01 -1 2025.12.31 -Nproc 4 > lowpass_log2025.log
--- extract/lowpass/run_lowpass_aurora.sh
+++ extract/lowpass/run_lowpass_aurora.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # code to create lowpass files for new LO with hourly data 2017 (Aurora's roms out), 2024 and 2025 (parker's roms out) (hourly data)
+# PM or AL, will need to update their get_lo_info to point to aurora's LO_roms, here roms_out5
 
 LOe=/dat1/parker/LO/extract/lowpass
 
-python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11b -ro 3 -0 2017.01.01 -1 2017.12.31 -Nproc 4 > lowpass_log2017.log
+python3 $LOe/extract_lowpass.py -gtx cas7_t1_x11b -ro 5 -0 2017.01.01 -1 2017.12.31 -Nproc 4 > lowpass_log2017.log
